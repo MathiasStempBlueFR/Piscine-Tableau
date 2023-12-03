2641aa774d2b2c497d434e94eb0f1fa7f6f6470f
MathiasStempBlueFR <147594943+MathiasStempBlueFR@users.noreply.github.com>
2023-12-03 13:52:30 +0100

    Ajout des boutons de gauche + comment + redimention du logiciel

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import tkinter as tk 
 from tkinter import ttk
 
-fenetre =tk.Tk()
+fenetre =tk.Tk() #début de l'iterface
+
+#Grid de gauche | zone d'ajout d'information
+B1 = tk.Button(fenetre, text="Ajout d'une longueur : ", fg="black", bg="lightgrey")
+B1.grid(column=0, row=0)
 
 
-fenetre.geometry("300x200")
+B2 = tk.Button(fenetre, text="Ajout d'un nageur : ", fg="black", bg="lightgrey")
+B2.grid(column=0, row=1)
+
+
+B3 = tk.Button(fenetre, text="Ajout d'un type de nage : ", fg="black", bg="lightgrey")
+B3.grid(column=0, row=2)
+
+
+
+fenetre.geometry("700x500") #fin de l'interface
 fenetre.mainloop()
